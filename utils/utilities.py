e13c31a4dd1582315d7bcfec6816649468c70515
prathit06 <malik.prathit@gmail.com>
2021-05-09 11:42:04 +0530

    code changes for cwl

--- utils/utilities.py
+++ utils/utilities.py
@@ -88,62 +88,62 @@ async def insertRecordsInDb_CWL(clantag, client):
         insertRecords = {}
         row_count = 0
 
-        async for war in group.get_wars():
-            if(war.clan_tag == '#92YQU2C'):
-                for attack in war.attacks:
-                    player_info = await client.get_player(attack.attacker_tag)
-                    if(player_info.clan.name == 'IMAGINE DRAGONS'):
-                        try:
-                            insertRecords['season'] = latest_season
-                        except:
-                            insertRecords['season'] = " "
-                        try:
-                            insertRecords['startTime'] = war.start_time.time
-                        except:
-                            insertRecords['startTime'] = " "
-                        try:
-                            insertRecords['endTime'] = war.end_time.time
-                        except:
-                            insertRecords['endTime'] = " "
-                        try:
-                            insertRecords['player_name'] = player_info.name
-                        except:
-                            insertRecords['player_name'] = " "
-                        try:
-                            insertRecords['player_townhalllevel'] = player_info.town_hall
-                        except:
-                            insertRecords['player_townhalllevel'] = " "
-                        try:
-                            insertRecords['stars'] = attack.stars
-                        except:
-                            insertRecords['stars'] = " "
-                        try:
-                            insertRecords['destruction'] = attack.destruction
-                        except:
-                            insertRecords['destruction'] = " "
+        async for war in group.get_wars_for_clan('#92YQU2C'):
+            # if(war.clan_tag == '#92YQU2C'):
+            for attack in war.attacks:
+                player_info = await client.get_player(attack.attacker_tag)
+                if(player_info.clan.name == 'IMAGINE DRAGONS'):
+                    try:
+                        insertRecords['season'] = latest_season
+                    except:
+                        insertRecords['season'] = " "
+                    try:
+                        insertRecords['startTime'] = war.start_time.time
+                    except:
+                        insertRecords['startTime'] = " "
+                    try:
+                        insertRecords['endTime'] = war.end_time.time
+                    except:
+                        insertRecords['endTime'] = " "
+                    try:
+                        insertRecords['player_name'] = player_info.name
+                    except:
+                        insertRecords['player_name'] = " "
+                    try:
+                        insertRecords['player_townhalllevel'] = player_info.town_hall
+                    except:
+                        insertRecords['player_townhalllevel'] = " "
+                    try:
+                        insertRecords['stars'] = attack.stars
+                    except:
+                        insertRecords['stars'] = " "
+                    try:
+                        insertRecords['destruction'] = attack.destruction
+                    except:
+                        insertRecords['destruction'] = " "
 
-                        try:
-                            con = lite.connect('test.db')
-                            cur = con.cursor()
+                    try:
+                        con = lite.connect('test.db')
+                        cur = con.cursor()
 
-                            cur.execute("""
-                            INSERT OR REPLACE INTO cwl_war_attacks(startTime, endTime, season, player_name, player_townhalllevel, stars, destruction)
-                            VALUES (:startTime, :endTime, :season, :player_name, :player_townhalllevel, :stars, :destruction)
-                            on conflict (season,startTime,player_name) do
-                            update set stars = {}, destruction = {}, update_timestamp = CURRENT_TIMESTAMP
-                            """.format(insertRecords['stars'], insertRecords['destruction']), insertRecords)
+                        cur.execute("""
+                        INSERT OR REPLACE INTO cwl_war_attacks(startTime, endTime, season, player_name, player_townhalllevel, stars, destruction)
+                        VALUES (:startTime, :endTime, :season, :player_name, :player_townhalllevel, :stars, :destruction)
+                        on conflict (season,startTime,player_name) do
+                        update set stars = {}, destruction = {}, update_timestamp = CURRENT_TIMESTAMP
+                        """.format(insertRecords['stars'], insertRecords['destruction']), insertRecords)
 
-                            row_count = row_count + cur.rowcount
-                            con.commit()
+                        row_count = row_count + cur.rowcount
+                        con.commit()
+                        con.close()
+
+                    except Exception as e:
+                        logging.error("Error {}:".format(e))
+                        sys.exit(1)
+                    finally:
+                        if con:
                             con.close()
 
-                        except Exception as e:
-                            logging.error("Error {}:".format(e))
-                            sys.exit(1)
-                        finally:
-                            if con:
-                                con.close()
-
         logging.info("upserted {} rows in cwl_war_attacks table !".format(row_count))
-    except:
-        logging.error("No CWL league as of now")
+    except Exception as e:
+        logging.error("Error occured :", e)
