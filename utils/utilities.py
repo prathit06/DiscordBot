89229a462d5f9552a7eff2c31232267900ca673a
prathit06 <malik.prathit@gmail.com>
2021-05-28 15:32:21 +0530

    code changes to exclude stars for friendly wars

--- utils/utilities.py
+++ utils/utilities.py
@@ -87,73 +87,79 @@ async def insertRecordsInDb_normal_wars(clantag, client):
         row_count = 0
 
         war_info = await client.get_clan_war('#92YQU2C')
-        for attack in war_info.attacks:
-            player_info = await client.get_player(attack.attacker_tag)
-            if player_info.clan is None:
-                pass
-            elif(player_info.clan.name == 'IMAGINE DRAGONS'):
-                try:
-                    insertRecords['season'] = latest_season
-                except Exception as e:
-                    logging.error("Exception in season :", e)
-                    insertRecords['season'] = " "
-                try:
-                    insertRecords['startTime'] = str(war_info.start_time.time)
-                except Exception as e:
-                    logging.error("Exception in startTime :", e)
-                    insertRecords['startTime'] = " "
-                try:
-                    insertRecords['endTime'] = str(war_info.end_time.time)
-                except Exception as e:
-                    logging.error("Exception in endTime :", e)
-                    insertRecords['endTime'] = " "
-                try:
-                    insertRecords['player_name'] = player_info.name
-                except Exception as e:
-                    logging.error("Exception in player_name :", e)
-                    insertRecords['player_name'] = " "
-                try:
-                    insertRecords['player_townhalllevel'] = player_info.town_hall
-                except Exception as e:
-                    logging.error("Exception in player_townhalllevel :", e)
-                    insertRecords['player_townhalllevel'] = " "
-                try:
-                    insertRecords['stars'] = attack.stars
-                except Exception as e:
-                    logging.error("Exception in stars :", e)
-                    insertRecords['stars'] = " "
-                try:
-                    insertRecords['destruction'] = attack.destruction
-                except Exception as e:
-                    logging.error("Exception in destruction :", e)
-                    insertRecords['destruction'] = " "
-                try:
-                    insertRecords['defender_tag'] = attack.defender_tag
-                except Exception as e:
-                    logging.error("Exception in defender_tag :", e)
-                    insertRecords['defender_tag'] = " "
+        war_type = war_info.type
 
-                try:
-                    con = psycopg2.connect(postgre_conn_uri)
-                    cur = con.cursor()
-                    query = """
-                    insert into normal_war_attacks(""" + ','.join(list(insertRecords.keys()))+""")
-                    values  """ + str(tuple(insertRecords.values())) + """
-                    on conflict(season,startTime,player_name,stars,destruction,defender_tag)
-                    do nothing
-                    """
-                    cur.execute(query)
+        if war_type == "random":
+            logging.info("Random war ongoing")
+            for attack in war_info.attacks:
+                player_info = await client.get_player(attack.attacker_tag)
+                if player_info.clan is None:
+                    pass
+                elif(player_info.clan.name == 'IMAGINE DRAGONS'):
+                    try:
+                        insertRecords['season'] = latest_season
+                    except Exception as e:
+                        logging.error("Exception in season :", e)
+                        insertRecords['season'] = " "
+                    try:
+                        insertRecords['startTime'] = str(war_info.start_time.time)
+                    except Exception as e:
+                        logging.error("Exception in startTime :", e)
+                        insertRecords['startTime'] = " "
+                    try:
+                        insertRecords['endTime'] = str(war_info.end_time.time)
+                    except Exception as e:
+                        logging.error("Exception in endTime :", e)
+                        insertRecords['endTime'] = " "
+                    try:
+                        insertRecords['player_name'] = player_info.name
+                    except Exception as e:
+                        logging.error("Exception in player_name :", e)
+                        insertRecords['player_name'] = " "
+                    try:
+                        insertRecords['player_townhalllevel'] = player_info.town_hall
+                    except Exception as e:
+                        logging.error("Exception in player_townhalllevel :", e)
+                        insertRecords['player_townhalllevel'] = " "
+                    try:
+                        insertRecords['stars'] = attack.stars
+                    except Exception as e:
+                        logging.error("Exception in stars :", e)
+                        insertRecords['stars'] = " "
+                    try:
+                        insertRecords['destruction'] = attack.destruction
+                    except Exception as e:
+                        logging.error("Exception in destruction :", e)
+                        insertRecords['destruction'] = " "
+                    try:
+                        insertRecords['defender_tag'] = attack.defender_tag
+                    except Exception as e:
+                        logging.error("Exception in defender_tag :", e)
+                        insertRecords['defender_tag'] = " "
 
-                    row_count = row_count + cur.rowcount
-                    con.commit()
-                    con.close()
+                    try:
+                        con = psycopg2.connect(postgre_conn_uri)
+                        cur = con.cursor()
+                        query = """
+                        insert into normal_war_attacks(""" + ','.join(list(insertRecords.keys()))+""")
+                        values  """ + str(tuple(insertRecords.values())) + """
+                        on conflict(season,startTime,player_name,stars,destruction,defender_tag)
+                        do nothing
+                        """
+                        cur.execute(query)
 
-                except Exception as e:
-                    logging.error("Error {}:".format(e))
-                    sys.exit(1)
-                finally:
-                    if con:
+                        row_count = row_count + cur.rowcount
+                        con.commit()
                         con.close()
+
+                    except Exception as e:
+                        logging.error("Error {}:".format(e))
+                        sys.exit(1)
+                    finally:
+                        if con:
+                            con.close()
+        else:
+            logging.info("Type of War is not random but {}".format(war_type))
 
         logging.info("upserted {} rows in normal_war_attacks table !".format(row_count))
     except Exception as e:
